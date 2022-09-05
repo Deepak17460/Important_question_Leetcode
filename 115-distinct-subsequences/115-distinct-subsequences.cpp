c30741dad564c4ee0ce03c73d9dbbd13609caa57
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-09-05 08:21:50 +0530

    Time: 85 ms (23.85%), Space: 18.5 MB (48.05%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,10 +1,12 @@
 class Solution {
     int Helper(string&s,string&t,int i,int j, vector<vector<int>>&dp){
         // Base Case
-        if(j<0) return 1;
-        if(i<0) return 0;
+        // if(j<0) return 1;
+        // if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
+        if(s[i-1]==t[j-1]){
              dp[i][j]=Helper(s,t,i-1,j-1,dp)+Helper(s,t,i-1,j,dp);
         }
         else
@@ -20,7 +22,7 @@ public:
         // Length of 2nd String
         int m=t.size();
         // Memoization
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return Helper(s,t,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return Helper(s,t,n,m,dp);
     }
 };
