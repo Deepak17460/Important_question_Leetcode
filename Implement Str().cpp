12c80e6d2d3e8f385cb8aa6b03a6a1a23d63f3c3
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-09-19 07:08:39 +0530

    Create Implement Str().cpp

---
+++ Implement Str().cpp
@@ -0,0 +1,13 @@
+ int strStr(string haystack, string needle) {   
+    int n=needle.size();
+    int l=0;
+    int m=haystack.size();
+    while(l<m){
+        string str="";
+        str=haystack.substr(l,n);
+        // checking
+        if(needle==str) return l;
+        l++;
+    }
+    return -1;
+    }
