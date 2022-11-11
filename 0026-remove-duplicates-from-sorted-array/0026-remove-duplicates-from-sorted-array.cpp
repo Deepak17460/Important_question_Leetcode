bc071e6ef31605ea257cd7bb1c32279b003dae3c
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-11-11 22:20:08 +0530

    Time: 75 ms (8.70%), Space: 18.4 MB (37.30%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,9 +5,8 @@ public:
         int id=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]!=nums[i-1]){
-                cout<<id<<endl;
-                nums[id++]=nums[i];
-                cout<<id<<endl;
+                nums[id]=nums[i];
+                id++;
             }
         }
         for(int i:nums) cout<<i<<" ";
