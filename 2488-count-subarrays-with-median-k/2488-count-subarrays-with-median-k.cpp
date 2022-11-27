17748eff8772ab4539a87c0a7648d34f83badb5e
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-11-27 12:00:21 +0530

    Time: 145 ms (87.50%), Space: 54.6 MB (50.00%) - LeetHub

--- 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
@@ -1,15 +1,11 @@
 #define pd push_back
-
+#define ll long long
 class Solution {
    
 public:
     int countSubarrays(vector<int>& nums, int k) {
-         int ans = 0;
-        int f = 0;
-        
-        int n = nums.size();
-    
-        
+         ll res = 0;
+        ll n = nums.size();
         for(int i=0;i<n;i++)
         {
              if(nums[i] == k) nums[i] = 1e5;
@@ -17,27 +13,27 @@ public:
              else nums[i] = 1;
         }
         
-        unordered_map<int,int> mp;
+        unordered_map<ll,ll> mp;
         k = 1e5;
-        int sum =0;
-        for(auto i:nums){
+        ll sum =0;
+        for(int i:nums){
             sum+=i;
             
-            if(sum==k) ans++;
+            if(sum==k) res++;
             if(mp.find(sum - k)!= mp.end()) 
             {
-                ans+= mp[sum - k];
+                res+= mp[sum - k];
             }
             
-            if(sum == k-1) ans++;
+            if(sum == k-1) res++;
             if(mp.find(sum - (k-1))!= mp.end())
             {
-                ans+= mp[sum - (k-1)];
+                res+= mp[sum - (k-1)];
             }
             
             mp[sum]++;
         }
         
-        return ans;
+        return res;
     }
 };
