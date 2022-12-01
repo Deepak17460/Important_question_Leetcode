edf457d07fa8fff278e21d1fcee39008b0f43cba
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-12-01 11:04:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (83.41%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        string a,b;
+       // string a,b;
         int n=s.size();
-        a=s.substr(0,n>>1);
-        b=s.substr(n>>1,n);
+        // a=s.substr(0,n>>1);
+        // b=s.substr(n>>1,n);
         int ct1=0,ct2=0;
-        for(char c:a){
-            if(c=='a'||c=='i'||c=='o'||c=='u'||c=='e'||c=='A'||c=='I'||c=='O'||c=='U'||c=='E') ct1++;
+        for(int i=0;i<n/2;i++){
+            if(s[i]=='a'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='e'||s[i]=='A'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='E')                     ct1++;
         }
-         for(char c:b){
-            if(c=='a'||c=='i'||c=='o'||c=='u'||c=='e'||c=='A'||c=='I'||c=='O'||c=='U'||c=='E') ct2++;
+         for(int i=n/2;i<n;i++){
+            if(s[i]=='a'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='e'||s[i]=='A'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='E')                     ct2++;
         }
         return ct1==ct2;
     }
