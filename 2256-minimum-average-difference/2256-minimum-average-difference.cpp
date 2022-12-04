18d0ef9cc9b39f996917f218e4c18303aebeb9f9
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-12-04 12:41:55 +0530

    Time: 257 ms (61.91%), Space: 79.2 MB (51.49%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -4,27 +4,24 @@ class Solution {
     int Helper(vector<int>&nums){
        ll n=nums.size();
         if(n==1)   return 0;
-      ll pre[n]; //to store prefix sum
+      ll pre[n]; 
       pre[0]=nums[0];
 	
-	//calculating prefix sum
     for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i];
 	
-    ll res=INT_MAX;
+    ll ans=1e9;
     ll ind=0;
 	
-	// calculating minimum average for i=0 to i=n-2
     for(int i=1;i<n;i++) {
         ll k=(abs(pre[i-1]/i-(pre[n-1]-pre[i-1])/(n-i)));
-        if(res>k){
-            res=min(res,k);
+        if(ans>k){
+            ans=min(ans,k);
             ind=i-1;
         }
     }
 	
-	//special case for i=n-1
-    if(res>abs(pre[n-1]/n)){
-        res=abs(pre[n-1]/n);
+    if(ans>abs(pre[n-1]/n)){
+        ans=abs(pre[n-1]/n);
         ind=n-1;
     }
     return ind;
